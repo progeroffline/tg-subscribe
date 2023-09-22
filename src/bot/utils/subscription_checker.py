91526aa791664af411392409e20b4a715b40b895
ProgerOffline <70508510+progeroffline@users.noreply.github.com>
2023-09-22 16:59:05 +0300

    Update subscription_checker.py
    Remove debug info

--- src/bot/utils/subscription_checker.py
+++ src/bot/utils/subscription_checker.py
@@ -12,7 +12,6 @@ async def task(bot: 'aiogram.Bot'):
    records = await transactions.get_new()
    for record in records:
       if await tronscan_service.check_transaction_for_correct_data(record.txid):
-         print(f'Transaction is valid {record.txid}, {type(record.created_at_timestamp)}')
          await transactions.set_status(True, database_id=record.id)
          await users.set_days_sub_end(
             count_days=NUMBER_DAYS_FROM_ONE_PAYMENT,
@@ -22,6 +21,4 @@ async def task(bot: 'aiogram.Bot'):
          await bot.send_message(
             chat_id=record.owner_telegram_id,
             text='Congratulations, you now have access to limited functionality.'
-         )
-      else:
-         print(f'Transaction not valid {record.txid}, {type(record.created_at_timestamp)}')
+         
