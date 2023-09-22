703598deab1f07056b93b710a1557bbe2f52c550
ProgerOffline <progeroffline@gmail.com>
2023-09-22 23:21:05 +0300

    Move private channels to close functionality section

--- src/bot/handlers/__init__.py
+++ src/bot/handlers/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from .channels_join_requests import *
+from .close_functionality import *
 from .payment import *
 from .start import *
--- src/bot/utils/subscription_checker.py
+++ src/bot/utils/subscription_checker.py
@@ -9,16 +9,16 @@ if typing.TYPE_CHECKING:
 
    
 async def task(bot: 'aiogram.Bot'):
-   records = await transactions.get_new()
-   for record in records:
-      if await tronscan_service.check_transaction_for_correct_data(record.txid):
-         await transactions.set_status(True, database_id=record.id)
-         await users.set_days_sub_end(
-            count_days=NUMBER_DAYS_FROM_ONE_PAYMENT,
-            telegram_id=record.owner_telegram_id,
-         ) 
-         
-         await bot.send_message(
-            chat_id=record.owner_telegram_id,
-            text='Congratulations, you now have access to limited functionality.'
+    records = await transactions.get_new()
+    for record in records:
+        if await tronscan_service.check_transaction_for_correct_data(record.txid):
+            await transactions.set_status(True, database_id=record.id)
+            await users.set_days_sub_end(
+                count_days=NUMBER_DAYS_FROM_ONE_PAYMENT,
+                telegram_id=record.owner_telegram_id,
+            ) 
+            await bot.send_message(
+                chat_id=record.owner_telegram_id,
+                text='Congratulations, you now have access to limited functionality.'
+            )
          
